76c5e795d307d8a090bdac6641f5c4b113aa4a3c
Ahleeem <kiheliahlem2@gmail.com>
2024-12-25 20:55:17 +0100

    update load_users

--- qcm_app.py
+++ qcm_app.py
@@ -4,10 +4,10 @@ import json
 
 def load_users(file="users.json"):
     try:
-       
-       
-       
-       
+       with open(file, 'r', encoding='utf-8') as f:
+            content = f.read().strip()
+            if not content:
+                return {"users": []}
             return json.loads(content)
     except FileNotFoundError:
         return {"users": []}

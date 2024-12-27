dfda0c76ea019c38be152e3265fe056233b25018
Lynaaa-05 <bentoumilynaracha@gmail.com>
2024-12-27 22:58:16 +0100

    find users and Authenticate existing user

--- qcm_app.py
+++ qcm_app.py
@@ -19,3 +19,36 @@ def load_users(file="users.json"):
 def save_users(users, file="users.json"):
     with open(file, 'w', encoding='utf-8') as f:
         json.dump(users, f, indent=4)
+
+# Find user 
+
+def find_user(username, users):
+    for user in users["users"]:
+        if user["username"] == username:
+            return user
+    return None
+
+
+# Register a new user
+
+def register_user(username, users):
+    password = input("Set your password: ").strip()
+    users["users"].append({"username": username, "password": password, "history": []})
+    save_users(users)
+    print(f"Account created for {username}.")
+
+
+# Authenticate existing user
+
+def authenticate_user(username, users):
+    user = find_user(username, users)
+    if not user:
+        print("User not found. Please register.")
+        return None
+    password = input("Enter your password: ").strip()
+    if password == user["password"]:
+        print(f"Welcome back, {username}!")
+        return user
+    else:
+        print("Incorrect password.")
+        return None
